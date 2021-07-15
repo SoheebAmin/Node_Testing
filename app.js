f2497ecde77e52495cdb67a0b64a98fc47ac6e3e
Soheeb Amin <soheeb@gmail.com>
2021-07-14 21:17:25 -0400

    all test functions together

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+const path = require('path')
+
+var pathObj = path.parse(__filename);
+
+console.log(pathObj);
+
+const os = require('os');
+
+var totalMem = os.totalmem();
+var freeMem = os.freemem();
+
+console.log("Total Memory " + totalMem);
+console.log("Free Memory: " + freeMem); 
+
 const EventEmitter = require('events');
 
 const emitter = new EventEmitter();
